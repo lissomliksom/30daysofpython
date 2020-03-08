20b1335837b8d5c0b54d05d3121a7aa8b924cfda
lissomliksom <lissomliksom@gmail.com>
2020-03-08 21:17:54 +0100

    Day 8

---
+++ PythonCrashCourse/Chapter6/6.9.py
@@ -0,0 +1,9 @@
+favorite_planets = {
+  'Simen': ['Venus', 'Mars', 'Jupiter'],
+  'Erlend': ['Tellus'],
+  'Tormod': ['Uranus', 'Neptune']
+}
+
+print("\nOur favorite planets:")
+for person in favorite_planets:
+  print(f"{person}: {favorite_planets[person]}")
